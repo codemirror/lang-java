996bba9b3b74e419b69a72c7a6cb9fcedc0a869f
Marijn Haverbeke <marijn@haverbeke.nl>
2020-12-29 13:49:45 +0100

    Strip /next/ from imported package names

--- src/java.ts
+++ src/java.ts
@@ -1,6 +1,6 @@
 import {parser} from "lezer-java"
-import {flatIndent, continuedIndent, indentNodeProp, foldNodeProp, LezerLanguage, LanguageSupport} from "@codemirror/next/language"
-import {styleTags, tags as t} from "@codemirror/next/highlight"
+import {flatIndent, continuedIndent, indentNodeProp, foldNodeProp, LezerLanguage, LanguageSupport} from "@codemirror/language"
+import {styleTags, tags as t} from "@codemirror/highlight"
 
 /// A language provider based on the [Lezer Java
 /// parser](https://github.com/lezer-parser/java), extended with
