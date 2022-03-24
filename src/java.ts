798e9cd48a99612b789df3806321562e830ecd71
Marijn Haverbeke <marijn@haverbeke.nl>
2022-03-24 09:55:44 +0100

    Move highlighting information into @lezer/java

--- src/java.ts
+++ src/java.ts
@@ -1,7 +1,6 @@
 import {parser} from "@lezer/java"
 import {flatIndent, continuedIndent, indentNodeProp, foldNodeProp, foldInside,
         LRLanguage, LanguageSupport} from "@codemirror/language"
-import {styleTags, tags as t} from "@codemirror/highlight"
 
 /// A language provider based on the [Lezer Java
 /// parser](https://github.com/lezer-parser/java), extended with
@@ -24,42 +23,6 @@ export const javaLanguage = LRLanguage.define({
         ["Block SwitchBlock ClassBody ElementValueArrayInitializer ModuleBody EnumBody " +
          "ConstructorBody InterfaceBody ArrayInitializer"]: foldInside,
         BlockComment(tree) { return {from: tree.from + 2, to: tree.to - 2} }
-      }),
-      styleTags({
-        null: t.null,
-        instanceof: t.operatorKeyword,
-        this: t.self,
-        "new super assert open to with void": t.keyword,
-        "class interface extends implements enum": t.definitionKeyword,
-        "module package import": t.moduleKeyword,
-        "switch while for if else case default do break continue return try catch finally throw": t.controlKeyword,
-        ["requires exports opens uses provides public private protected static transitive abstract final " +
-          "strictfp synchronized native transient volatile throws"]: t.modifier,
-        IntegerLiteral: t.integer,
-        FloatLiteral: t.float,
-        "StringLiteral TextBlock": t.string,
-        CharacterLiteral: t.character,
-        LineComment: t.lineComment,
-        BlockComment: t.blockComment,
-        BooleanLiteral: t.bool,
-        PrimitiveType: t.standard(t.typeName),
-        TypeName: t.typeName,
-        Identifier: t.variableName,
-        "MethodName/Identifier": t.function(t.variableName),
-        Definition: t.definition(t.variableName),
-        ArithOp: t.arithmeticOperator,
-        LogicOp: t.logicOperator,
-        BitOp: t.bitwiseOperator,
-        CompareOp: t.compareOperator,
-        AssignOp: t.definitionOperator,
-        UpdateOp: t.updateOperator,
-        Asterisk: t.punctuation,
-        Label: t.labelName,
-        "( )": t.paren,
-        "[ ]": t.squareBracket,
-        "{ }": t.brace,
-        ".": t.derefOperator,
-        ", ;": t.separator
       })
     ]
   }),
